369e5621787e3dab5931b6e77515f7b0bd63ac68
LIN <yi19930716@gmail.com>
2020-07-30 23:15:00 +0800

    feat: use session

--- routes/models/home.js
+++ routes/models/home.js
@@ -1,18 +1,28 @@
 const express = require('express')
 const User = require('../../models/user.js')
 const router = express.Router()
-const cookieParser = require('cookie-parser')
-router.use(cookieParser())
+const session = require('express-session') //載入express-session
+const MongoStore = require('connect-mongo')(session); //載入connect-mongo
+router.use(session({
+  secret: 'hello',
+  resave: true,
+  rolling: false,
+  saveUninitialized: true,
+  store: new MongoStore({ url: 'mongodb://localhost:27017/sessiondb' }), //使用mongodb儲存
+  cookie: { httpOnly: true, maxAge: 5000 } //30秒清除session
+}))
 
 
 router.get('/', (req, res) => {
-  const alert = req.cookies.alert
-  res.render('index', { alert })
+  const { user, alert, email, password } = req.session
+  if (user) { //驗證user session是否還在(登入狀態),若還在轉跳login路由
+    return res.redirect('/login')
+  }
+  res.render('index', { alert, email, password }) //若user session不存在,則顯示登入畫面
 })
 
 router.post('/', (req, res) => {
-  res.clearCookie('user', { httpOnly: true })
-  res.clearCookie('alert', { httpOnly: true })
+  req.session.destroy() //銷毀session
   res.redirect('/')
 })
 
--- routes/models/login.js
+++ routes/models/login.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const User = require('../../models/user.js')
-const cookieParser = require('cookie-parser')
 const router = express.Router()
-router.use(cookieParser())
 
 router.get('/', (req, res) => {
-  const firstName = req.cookies.user
+  const firstName = req.session.user
   res.render('loginPass', { firstName })
 })
 
@@ -14,13 +12,12 @@ router.post('/', (req, res) => {
   User.find({ email: `${email}`, password: `${password}` })
     .then((user) => {
       if (user.length === 0) {
-        const alert = `Email or Password error!`
-        res.cookie('alert', alert)
+        req.session.alert = `Email or Password error!` //設置session
+        req.session.email = email
+        req.session.password = password
         return res.redirect('/')
       } else {
-        //maxAge:設定登入時間後多久清除cookie,單位(ms),此設置1分鐘
-        //如果maxAge沒有設定,關掉瀏覽器cookie直接清除
-        res.cookie("user", user[0].firstName, { httpOnly: true, maxAge: 60000 })
+        req.session.user = user[0].firstName
         return res.redirect('/login')
       }
     })
